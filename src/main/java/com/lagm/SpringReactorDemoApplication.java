6fbb1962f6bb6e2dc18e3008058de0e7415dc4cc
Luis Guisado <lguisadom@gmail.com>
2023-04-22 23:09:31 -0500

    First commit

---
+++ src/main/java/com/lagm/SpringReactorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lagm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagm/SpringReactorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
